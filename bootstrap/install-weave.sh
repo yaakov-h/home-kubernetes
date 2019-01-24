0e6d4f43be9e507eb51c317116c976688b012e27
Yaakov <git@yaakov.online>
2019-01-24 22:50:28 +1100

    Fix deployment issues to new machine

--- bootstrap/install-weave.sh
+++ bootstrap/install-weave.sh
@@ -7,12 +7,12 @@
 
 kubectl apply -f bootstrap/flux-namespace.yaml
 
-if [ ! -f ../secrets/sealed-secrets-master-key.yaml ]; then
+if [ ! -f secrets/sealed-secrets-master-key.yaml ]; then
     echo "Sealed Secrets master key not found. Aborting..."
     exit -1
 fi
 
-kubectl apply -f ../secrets/sealed-secrets-master-key.yaml
+kubectl apply -f secrets/sealed-secrets-master-key.yaml
 
 KUBESEAL_VERSION="v0.7.0"
 kubectl create -f https://github.com/bitnami-labs/sealed-secrets/releases/download/${KUBESEAL_VERSION}/sealedsecret-crd.yaml
