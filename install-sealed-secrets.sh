9d2143141c1b1fa1decae7fa4ec0ee72eedd1bab
Yaakov <git@yaakov.online>
2020-04-27 11:03:27 +0000

    Update Sealed Secrets

--- install-sealed-secrets.sh
+++ install-sealed-secrets.sh
@@ -3,7 +3,7 @@
 # Use an older version of the client tooling
 # until issue #86 is fixed.
 # https://github.com/bitnami-labs/sealed-secrets/issues/86#issuecomment-421556209
-release="v0.5.1"
+release="v0.12.1"
 
 # Install client tooling
 GOOS=$(go env GOOS)
